64eff1fa894bda722ed4cfaeb153d9b8277ecff5
Harleen Kaur Saini <harleensaini2016@gmail.com>
2021-05-22 04:07:06 +0530

    Fully Working JS Code

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -23,9 +23,9 @@ let scores = [{userName : "harleen1", userID : "1", group: "abcd", day : "1", mo
                     {userName : "harleensdf", userID : "5", group: "abcd", day : "1", month : "1", year:"2020", emission:"20"},
                     {userName : "harleensdf", userID : "5", group: "abcd", day : "1", month : "1", year:"2020", emission:"20"},];
 
-let usersInGroup = [];
+// let usersInGroup = [];
 
-function updateLeaderboardView() {
+function updateLeaderboardView(records) {
     // let records = [];
     // // Get the values from database
     
@@ -41,64 +41,81 @@ function updateLeaderboardView() {
     //                     emission : doc.data().emission});
     //     })
     
-    records = userIDToEmission;
-        // Process the data to be in the form of {name: "name", emission: "234"}.
-        let uniqueIndivisuals = new Map();
-        for(let i=0; i<records.length ;i++){
-            // console.log(records[i]);
-            if(uniqueIndivisuals.has(records[i].userID)){
-                uniqueIndivisuals.set(records[i].userID, 
-                                        [records[i].userID, 
-                                            parseInt(records[i].emission) + 
-                                            parseInt(uniqueIndivisuals.get(records[i].userID)[1])]);
-            }else{
-                uniqueIndivisuals.set(records[i].userID, 
-                    [records[i].userName, parseInt(records[i].emission)]);
-            }
-        }
+    // records = userIDToEmission;
+    // records = scores;
+    // Process the data to be in the form of {name: "name", emission: "234"}.
+    // let uniqueIndivisuals = new Map();
+    // for(let i=0; i<records.length ;i++){
+    //     // console.log(records[i]);
+    //     if(uniqueIndivisuals.has(records[i].userID)){
+    //         uniqueIndivisuals.set(records[i].userID, 
+    //                                 [records[i].userName, 
+    //                                     parseInt(records[i].emission) + 
+    //                                     parseInt(uniqueIndivisuals.get(records[i].userID)[1])]);
+    //     }else{
+    //         uniqueIndivisuals.set(records[i].userID, 
+    //             [records[i].userName, parseInt(records[i].emission)]);
+    //     }
+    // }
 
-        let scores = [];
-        uniqueIndivisuals.forEach((values, key) => {
-            scores.push({
-                name : values[0],
-                score : values[1],
-            });
-        });
-
-        // console.log(scores)
-        let leaderboard = document.getElementById("leaderboard");
-        leaderboard.innerHTML = "";
-
-        scores.sort(function(a, b){ return b.score - a.score  });
-        let elements = []; // we'll need created elements to update colors later on
-        // create elements for each player
-        for(let i=0; i<scores.length; i++) {
-            let name = document.createElement("div");
-            let score = document.createElement("div");
-            name.className = "name_bar";
-            name.innerText = i+1 + ". " + scores[i].name;
-            score.innerText = scores[i].score;
-
-            let scoreRow = document.createElement("div");
-            scoreRow.classList.add("lboard_mem");
-            scoreRow.appendChild(name);
-            scoreRow.appendChild(score);
-            leaderboard.appendChild(scoreRow);
-
-            elements.push(scoreRow);
-
-        }
+    // let scores = [];
+    // uniqueIndivisuals.forEach((values, key) => {
+    //     scores.push({
+    //         name : values[0],
+    //         score : values[1],
+    //     });
     // });
+
+    // // console.log(scores)
+    // let leaderboard = document.getElementById("leaderboard");
+    // leaderboard.innerHTML = "";
+
+    // scores.sort(function(a, b){ return b.score - a.score  });
+    // let elements = []; // we'll need created elements to update colors later on
+    // // create elements for each player
+    // for(let i=0; i<scores.length; i++) {
+    //     let name = document.createElement("div");
+    //     let score = document.createElement("div");
+    //     name.className = "name_bar";
+    //     name.innerText = i+1 + ". " + scores[i].name;
+    //     score.innerText = scores[i].score;
+    //     let scoreRow = document.createElement("div");
+    //     scoreRow.classList.add("lboard_mem");
+    //     scoreRow.appendChild(name);
+    //     scoreRow.appendChild(score);
+    //     leaderboard.appendChild(scoreRow);
+    //     elements.push(scoreRow);
+    // }
 }
 
-function updateLeaderboardViewGroup() {
+let uniqueIndivisuals = new Map();
+
+function getUniquePlayers(value){
+    console.log(value)
+    if(uniqueIndivisuals.has(records[i].userID)){
+        uniqueIndivisuals.set(records[i].userID, 
+                                [records[i].userName, 
+                                    parseInt(records[i].emission) + 
+                                    parseInt(uniqueIndivisuals.get(records[i].userID)[1])]);
+    }else{
+        uniqueIndivisuals.set(records[i].userID, 
+            [records[i].userName, parseInt(records[i].emission)]);
+    }
+}
+
+function updateLeaderboardViewGroup(records_1) {
+    // records_1.push({   userID: "S.No",
+    //     userName : "Name",
+    //     emission : "FootPrint"
+    // })
+    // console.log(records_1)
     // let records = [];
-    // // Get the values from database
+    // // // Get the values from database
     
     
     // db.collection('carbon_data').get().then((snapshot) => {
     //     snapshot.docs.forEach(doc => {
-    //         console.log(doc.data())
+    //         // console.log(doc.data())
     //         records.push({userName : doc.data().group,
     //                     userID : doc.data().group,
     //                     // group : doc.data().group,
@@ -107,80 +124,162 @@ function updateLeaderboardViewGroup() {
     //                     year : doc.data().year,
     //                     emission : doc.data().emission});
     //     })
+    //     console.log(records)
+    // })
     
-        console.log(userIDToEmission);
-        records = userIDToEmission;
-        // Process the data to be in the form of {name: "name", emission: "234"}.
-        let uniqueIndivisuals = new Map();
-        for(let i=0; i<records.length ;i++){
-            // console.log(records[i]);
-            if(uniqueIndivisuals.has(records[i].userID)){
-                uniqueIndivisuals.set(records[i].userID, 
-                                        [records[i].userID, 
-                                            parseInt(records[i].emission) + 
-                                            parseInt(uniqueIndivisuals.get(records[i].userID)[1])]);
-            }else{
-                uniqueIndivisuals.set(records[i].userID, 
-                    [records[i].userName, parseInt(records[i].emission)]);
-            }
-        }
+    // console.log("records: ", records);
+    // records = userIDToEmission;
+    // records = scores;
+    // records = userIDToEmission;
+    // Process the data to be in the form of {name: "name", emission: "234"}.
+    // let uniqueIndivisuals = new Map();
+    // for(let i=0; i<records.length ;i++){
+    // records.forEach()
+    //     console.log(records[i]);
+    //     if(uniqueIndivisuals.has(records[i].userID)){
+    //         uniqueIndivisuals.set(records[i].userID, 
+    //                                 [records[i].userName, 
+    //                                     parseInt(records[i].emission) + 
+    //                                     parseInt(uniqueIndivisuals.get(records[i].userID)[1])]);
+    //     }else{
+    //         uniqueIndivisuals.set(records[i].userID, 
+    //             [records[i].userName, parseInt(records[i].emission)]);
+    //     }
+    // }
 
-        let scores = [];
-        uniqueIndivisuals.forEach((values, key) => {
-            scores.push({
-                name : values[0],
-                score : values[1],
-            });
+    // console.log(records_1)
+    let uniqueIndivisuals = new Map();
+    records_1.forEach((value) => {
+        console.log(value)
+        if(uniqueIndivisuals.has(value.userID)){
+            uniqueIndivisuals.set(value.userID, 
+                                    [value.userName, 
+                                        parseInt(value.emission) + 
+                                        parseInt(uniqueIndivisuals.get(value.userID)[1])]);
+        }else{
+            uniqueIndivisuals.set(value.userID, 
+                [value.userName, parseInt(value.emission)]);
+        }
+    })
+    // console.log("uniqueIndivisuals : ", uniqueIndivisuals)
+    let scores = [];
+    uniqueIndivisuals.forEach((values, key) => {
+        scores.push({
+            name : values[0],
+            score : values[1],
         });
+    });
 
-        // console.log(scores)
-        let leaderboard = document.getElementById("leaderboard");
-        leaderboard.innerHTML = "";
+    console.log(scores);
 
-        scores.sort(function(a, b){ return b.score - a.score  });
-        let elements = []; // we'll need created elements to update colors later on
-        // create elements for each player
-        for(let i=0; i<scores.length; i++) {
-            let name = document.createElement("div");
-            let score = document.createElement("div");
-            name.className = "name_bar";
-            name.innerText = i+1 + ". " + scores[i].name;
-            score.innerText = scores[i].score;
+    //     // console.log(scores)
+    let leaderboard = document.getElementById("leaderboard");
+    leaderboard.innerHTML = "";
 
-            let scoreRow = document.createElement("div");
-            scoreRow.classList.add("lboard_mem");
-            scoreRow.appendChild(name);
-            scoreRow.appendChild(score);
-            leaderboard.appendChild(scoreRow);
+    scores.sort(function(a, b){ return b.score - a.score  });
+    let elements = []; // we'll need created elements to update colors later on
+    // create elements for each player
+    for(let i=0; i<scores.length; i++) {
+        let name = document.createElement("div");
+        let score = document.createElement("div");
+        name.className = "name_bar";
+        name.innerText = i+1 + ". " + scores[i].name;
+        score.innerText = scores[i].score;
+        let scoreRow = document.createElement("div");
+        scoreRow.classList.add("lboard_mem");
+        scoreRow.appendChild(name);
+        scoreRow.appendChild(score);
+        leaderboard.appendChild(scoreRow);
+        elements.push(scoreRow);
+    }
+}
 
-            elements.push(scoreRow);
+function report1(groupName){
+    let usersInGroup = [];
+    userIDToEmission = [];
 
-        }
+    db.collection('groups/').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            if(doc.data().name == groupName){
+                let M = doc.data().members // .forEach(getUserData);
+                M.forEach((value) => {
+                    db.collection('users/').doc(value).get().then((doc) => {
+                        userIDToEmission.push({
+                            userID: value,
+                            userName : doc.data().name,
+                            emission : doc.data().currentFootprint
+                        })
+                    })
+                });
+            }
+        })
+        console.log(userIDToEmission)
+        let uniqueIndivisuals = new Map();
+        userIDToEmission.forEach((value) => {
+            console.log(value)
+            if(uniqueIndivisuals.has(value.userID)){
+                uniqueIndivisuals.set(value.userID, 
+                                        [value.userName, 
+                                            parseInt(value.emission) + 
+                                            parseInt(uniqueIndivisuals.get(value.userID)[1])]);
+            }else{
+                uniqueIndivisuals.set(value.userID, 
+                    [value.userName, parseInt(value.emission)]);
+            }
+        })
+        console.log(userIDToEmission)
+        // updateLeaderboardViewGroup(userIDToEmission);
+    });
 }
 
 function report(groupName){
     let usersInGroup = [];
-    db.collection('groups/').get().then((snapshot) => {
+    db.collection('users/').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
-            if(doc.data().name == groupName){
-                let M = doc.data().groups
-                getUserData(M);
-                M.forEach(getUserData);
-            }
-        })
-    });
-}
-let userIDToEmission = [];
-function getUserData(userID_indivisual){
-    // console.log(userID_indivisual)
-    db.collection('users/'+userID_indivisual+'/calculations').get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            userIDToEmission.push({
-                userID : userID_indivisual,
-                emission : doc.data().food + doc.data().transport + doc.data().home
+            let M = doc.data().groups
+            // console.log(M)
+            M.forEach((value) => {
+                // console.log(doc.data().name, value, groupName)
+                if(new String(value).valueOf() == new String(groupName).valueOf()){
+                    // console.log("Here")
+                    usersInGroup.push({
+                        userName : doc.data().name,
+                        userID : doc.id,
+                        emission : doc.data().currentFootprint
+                    })
+                }
             })
+            
         })
-        updateLeaderboardViewGroup();
+        // console.log(usersInGroup)
+        updateLeaderboardViewGroup(usersInGroup);
+    })
+}
+// let userIDToEmission = [];
+// function getUserData(userID_indivisual){
+//     // console.log(userID_indivisual)
+//     let userIDToEmission = [];
+//     db.collection('users/'+userID_indivisual+'/calculations').get().then((snapshot) => {
+//         snapshot.docs.forEach(doc => {
+//             userIDToEmission.push({
+//                 userID : userID_indivisual,
+//                 emission : doc.data().food + doc.data().transport + doc.data().home
+//             })
+//         })
+//         updateLeaderboardViewGroup(userIDToEmission);
+//     })
+// }
+
+let userIDToEmission = [{userID: "S.No",
+    userName : "Name",
+    emission : "FootPrint"}];
+function getUserData(userID_indivisual){
+    db.collection('users/').doc(userID_indivisual).get().then((doc) => {
+        userIDToEmission.push({
+            userID: userID_indivisual,
+            userName : doc.data().name,
+            emission : doc.data().currentFootprint
+        });
     })
 }
 
