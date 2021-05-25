96866dbac5233ec96e028c6b510b61a0bacc89b8
Johnny Ryue <johnryue1993@gmail.com>
2021-05-25 14:24:56 -0700

    style changes to the universal.css file and initial work on JS functions for pull_incentives.js

--- scripts/pull_incentive.js
+++ scripts/pull_incentive.js
@@ -1,3 +1,43 @@
+/* jQuery(document).ready(function() {
+    let activeTab = $("#overview-tab a").filter(".active");
+    
+    $("#overview-tab a").click(function(e) {
+        e.preventDefault();
+
+        activeTab.removeClass('active');
+        $(activeTab.attr('href')).removeClass('active');
+
+        activeTab = $(this);
+
+        activeTab.addClass("active");
+        $(activeTab.attr('href')).addClass("active");
+    })
+}) */
+
+$(document).ready( function() {
+    $('#overview-tab').carousel({
+        interval:   4000
+    });
+
+    var clickEvent = false;
+    $('#myCarousel').on('click', '.nav a', function() {
+            clickEvent = true;
+            $('.nav li').removeClass('active');
+            $(this).parent().addClass('active');        
+    }).on('slid.bs.carousel', function(e) {
+        if(!clickEvent) {
+            var count = $('.nav li').length -1;
+            var current = $('.nav li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to')) +1;
+            if(count+1 == id) {
+                $('.nav li').first().addClass('active');    
+            }
+        }
+        clickEvent = false;
+    });
+});
+
 $("#incent-tab").ready(function () {
 
     const users = db.collection("users");
